e97d7573fda02e6e381b13cc85187815462c0f40
raxhvl <raxhvl@users.noreply.github.com>
2025-02-09 04:13:23 +0000

    ♻️ refactor(ui): Fetch challenges

--- src/app/challenges/[id]/page.tsx
+++ src/app/challenges/[id]/page.tsx
@@ -1,4 +1,8 @@
+"use client";
+import ProgressBar from "@/components/ProgressBar";
 import { use } from "react";
+import { useAppContext } from "../../context/AppContext";
+import { Trophy } from "lucide-react";
 
 export default function ChallengePage({
   params,
@@ -7,5 +11,21 @@ export default function ChallengePage({
 }) {
   const { id } = use(params);
   const challengeId = Number.parseInt(id);
-  return <div>Challenge {challengeId}</div>;
+
+  const { challenges } = useAppContext();
+  const challenge = challenges.find((c) => c.id === challengeId);
+
+  if (!challenge) {
+    return <div>Challenge not found!</div>;
+  }
+
+  return (
+    <div>
+      <ProgressBar currentChallengeId={challengeId} />
+      <h1 className="text-3xl font-bold mb-6 glow flex items-center">
+        <Trophy className="mr-2 h-8 w-8 text-yellow-400" />
+        {challenge.title}
+      </h1>
+    </div>
+  );
 }
